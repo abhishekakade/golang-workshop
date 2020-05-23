1ecff162f29564f6444656782f88d3104116e343
Abhishek K <abhishekakade@gmail.com>
2020-05-23 14:57:17 +0530

    feat: act2.01 fizzbuzz - added switch case with optimizations

--- act2.01/main.go
+++ act2.01/main.go
@@ -5,14 +5,42 @@ import (
 )
 
 func main() {
+
+	const (
+		FIZZ     = 3
+		BUZZ     = 5
+		FIZZBUZZ = 15
+	)
+
+	// With for loop
 	for i := 1; i < 101; i++ {
-		if i%3 == 0 && i%5 == 0 {
+		// below line works fine but directly using 15 will save some calculations and make it faster
+		// if i%FIZZ == 0 && i%BUZZ == 0 {
+		// so we will use that as an optimization
+		if i%FIZZBUZZ == 0 {
 			fmt.Println("FizzBuzz")
-		} else if i%3 == 0 {
+		} else if i%FIZZ == 0 {
 			fmt.Println("Fizz")
-		} else if i%5 == 0 {
+		} else if i%BUZZ == 0 {
 			fmt.Println("Buzz")
 		} else {
+			fmt.Println(i)
+		}
+	}
+
+	// With switch case
+	for i := 1; i < 101; i++ {
+		switch {
+		// below line works fine but directly using 15 will save some calculations and make it faster
+		// case ((i%FIZZ == 0) && (i%BUZZ == 0)):
+		// so we will use that as an optimization
+		case i%FIZZBUZZ == 0:
+			fmt.Println("FizzBuzz")
+		case i%FIZZ == 0:
+			fmt.Println("Fizz")
+		case i%BUZZ == 0:
+			fmt.Println("Buzz")
+		default:
 			fmt.Println(i)
 		}
 	}
